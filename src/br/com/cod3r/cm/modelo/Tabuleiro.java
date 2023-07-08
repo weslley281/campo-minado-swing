477e65671a267659f9792f443faf5d54bc178038
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-07-08 06:46:33 -0400

    a

--- src/br/com/cod3r/cm/modelo/Tabuleiro.java
+++ src/br/com/cod3r/cm/modelo/Tabuleiro.java
@@ -3,15 +3,17 @@ package br.com.cod3r.cm.modelo;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
-public class Tabuleiro {
+public class Tabuleiro implements CampoObservador {
 
 	private int linhas;
 	private int colunas;
 	private int minas;
 	
 	private final List<Campo> campos = new ArrayList<>();
+	private final List<Consumer<ResultadoEvento>> observadores = new ArrayList<>();
 
 	public Tabuleiro(int linhas, int colunas, int minas) {
 		this.linhas = linhas;
@@ -23,18 +25,20 @@ public class Tabuleiro {
 		sortearMinas();
 	}
 
-	public void abrir(int linha, int coluna) {
-		try {
-			campos.parallelStream()
-				.filter(c -> c.getLinha() == linha && c.getColuna() == coluna)
-				.findFirst()
-				.ifPresent(c -> c.abrir());
-		} catch (Exception e) {
-			// FIXME Ajustar a implementação do metodo abrir
-			campos.forEach(c -> c.setAberto(true));
-			throw e;
-		}
+	public void registrarObservador(Consumer<ResultadoEvento> observador){
+		observadores.add(observador);
 	}
+
+	public void notificarObservadores(boolean resultado){
+		observadores.stream().forEach(o -> o.accept(new ResultadoEvento(resultado)));
+	}
+
+	public void abrir(int linha, int coluna) {
+		campos.parallelStream().filter(c -> c.getLinha() == linha && c.getColuna() == coluna).findFirst().ifPresent(c -> c.abrir());
+
+	}
+
+
 
 	public void alternarMarcacao(int linha, int coluna) {
 		campos.parallelStream()
@@ -46,7 +50,9 @@ public class Tabuleiro {
 	private void gerarCampos() {
 		for (int linha = 0; linha < linhas; linha++) {
 			for (int coluna = 0; coluna < colunas; coluna++) {
-				campos.add(new Campo(linha, coluna));
+				Campo campo = new Campo(linha, coluna);
+				campo.registrarObservador(this);
+				campos.add(campo);
 			}
 		}
 	}
@@ -77,5 +83,20 @@ public class Tabuleiro {
 	public void reiniciar() {
 		campos.stream().forEach(c -> c.reiniciar());
 		sortearMinas();
+	}
+
+	@Override
+	public void eventoOcorreu(Campo campo, CampoEvento evento) {
+		if (evento == CampoEvento.EXPLODIR){
+			System.out.println("Perdeu");
+			notificarObservadores(false);
+		}else if (objetivoAlcancado()){
+			System.out.println("Ganhou");
+			notificarObservadores(true);
+		}
+	}
+
+	private void mostrarMinas{
+		campos.stream().filter(c -> c.isMinado()).forEach(c -> c.setAberto(true));
 	}
 }
---
+++ src/br/com/cod3r/cm/visao/Temp.java
@@ -0,0 +1,19 @@
+package br.com.cod3r.cm.visao;
+
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+public class Temp {
+    public static void main(String[] args) {
+        Tabuleiro tabuleiro = new Tabuleiro(3,3,9);
+        tabuleiro.abrir(0,0);
+        tabuleiro.abrir(0,1);
+        tabuleiro.abrir(0,2);
+        tabuleiro.abrir(1,0);
+        tabuleiro.abrir(1,1);
+        tabuleiro.abrir(1,2);
+        tabuleiro.abrir(2,0);
+        tabuleiro.abrir(2,1);
+        tabuleiro.abrir(2,2);
+
+    }
+}
