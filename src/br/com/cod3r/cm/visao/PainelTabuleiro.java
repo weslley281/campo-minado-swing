30ab1c829972e9a68a2db6109552e9e0c8d08267
Weslley Ferraz <weslleyhenrique800@gmail.com>
2023-07-09 10:14:20 -0400

    a

--- src/br/com/cod3r/cm/visao/PainelTabuleiro.java
+++ src/br/com/cod3r/cm/visao/PainelTabuleiro.java
@@ -1,4 +1,11 @@
 package br.com.cod3r.cm.visao;
 
-public class PainelTabuleiro {
+import br.com.cod3r.cm.modelo.Tabuleiro;
+
+import javax.swing.*;
+
+public class PainelTabuleiro extends JPanel {
+    public PainelTabuleiro(Tabuleiro tabuleiro){
+
+    }
 }
--- src/br/com/cod3r/cm/visao/TelaPrincipal.java
+++ src/br/com/cod3r/cm/visao/TelaPrincipal.java
@@ -9,6 +9,7 @@ public class TelaPrincipal extends JFrame {
     public TelaPrincipal() throws HeadlessException {
         Tabuleiro tabuleiro = new Tabuleiro(16,30,50);
 
+        add(new PainelTabuleiro(tabuleiro));
         setTitle("Campo Minado Cod3R");
         setSize(690, 438);
         setLocationRelativeTo(null);
